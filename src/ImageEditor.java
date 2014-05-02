e11738f41e45de427c6231f58a717c8174d3e49b
Larry114 <lf.zhang86@gmail.com>
2014-05-02 01:24:38 -0600

    Add Pixle class

--- src/ImageEditor.java
+++ src/ImageEditor.java
@@ -1,5 +1,19 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * Created by Lingfu on 5/2/2014.
  */
 public class ImageEditor {
+    private int width = 0;
+    private int height = 0;
+    private int maxColorValue = 255;
+    private Pixle image[][];
+    final String MAGIC_NUMBER = "P3";
+
+    public ImageEditor (File inputFile) throws FileNotFoundException {
+        Scanner scanner = new Scanner(inputFile);
+        String firstString = scanner.next();
+    }
 }
