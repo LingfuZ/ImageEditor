8fd5760059f545c23466cc0b62dcf9070b283c7a
Larry114 <lf.zhang86@gmail.com>
2014-05-02 00:16:55 -0600

    create project file structure

---
+++ src/ImageEditor.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Lingfu on 5/2/2014.
+ */
+public class ImageEditor {
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Lingfu on 5/2/2014.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Digitalicious!");
+    }
+}
